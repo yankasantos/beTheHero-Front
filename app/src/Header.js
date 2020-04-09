46ecec956250ea223e43b4aae0104d12140289cb
yankasantos <yankasantos0107@gmail.com>
2020-04-09 10:42:00 -0300

    utilizando o pops.children para acessar informações passadas ao componente por onde está sendo utilizado

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Header(props){
+function Header({children}){
+    /* essa alteração equivale ao props.chidren, que serve para acessar 
+     * o que esta sendo passado para o componente onde ele esta sendo usado
+     * */
     return(
         <header>
-            <h1>{props.title}</h1>
+            
+            <h1>{children}</h1>
         </header>
     );
 }
