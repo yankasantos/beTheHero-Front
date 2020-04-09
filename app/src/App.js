e3000453662261ba4afda703fb1ddd0ba4ed3460
yankasantos <yankasantos0107@gmail.com>
2020-04-09 11:39:05 -0300

    criando nossa primeira tela, e importando o componente para o app

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,10 @@
 import React, {useState} from 'react';
-import Header from './Header';
+import Logon from './pages/Logon';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-/** o useState ele manipula os valores atualizando o estado do valor anteriormente utilizando
-* um array[valor, atualizacao], com isso podemos usar uma constante preservando a imutabilidade dela
-**/
-  function increment(){
-    setCounter(counter + 1);
-  }
 
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
     
   );
 }
