5af9e99f0cfeaeff68dfd8a605cd5088e4a06076
yankasantos <yankasantos0107@gmail.com>
2020-04-29 21:40:36 -0300

    integração da api quando carrega a página já trazendo a  lista de incidentes para aquela ong logada

--- app/src/pages/Profiles/index.js
+++ app/src/pages/Profiles/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {FiPower, FiTrash2} from 'react-icons/fi';
 
+import api from '../../services/api';
 import './style.css';
 import logo from '../../assets/logo.svg';
 
 export default function Profile(){
+    const [incidents, setIncidents] = useState([]);
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers:{
+                Authorization:ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    } , [ongId]);
+
+   
+
     return(
         <div className="profile-container">
             <header>
                 <img src={logo} alt="logo Be The Hero, vermelho e preto"/>
-                <span>Bem vinda, APAD </span>
+                <span>Bem vinda, {ongName} </span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
                     <FiPower size={18} color="#a8a8b3"/>
@@ -20,61 +37,21 @@ export default function Profile(){
             <h1>Casos Cadastrados</h1>
 
             <ul>
-                <li>
-                    <strong>Caso: </strong>
-                    <p>Caso teste</p>
+                {incidents.map(incidents => (
+                    <li>
+                        <strong>Caso: </strong>
+                        <p>{incidents.title}</p>
 
-                    <strong>DESCRIÇÃO: </strong>
-                    <p>Descrição teste</p>
+                        <strong>DESCRIÇÃO: </strong>
+                        <p>{incidents.description}e</p>
 
-                    <strong>VALOR: </strong>
-                    <p>R$240,00</p>
-                    <button>
-                        <FiTrash2 size="20" color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Caso: </strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO: </strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR: </strong>
-                    <p>R$240,00</p>
-                    <button>
-                        <FiTrash2 size="20" color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Caso: </strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO: </strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR: </strong>
-                    <p>R$240,00</p>
-                    <button type="button">
-                        <FiTrash2 size="20" color="#a8a8b3" />
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Caso: </strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO: </strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR: </strong>
-                    <p>R$240,00</p>
-                    <button>
-                        <FiTrash2 size="20" color="#a8a8b3" />
-                    </button>
-                </li>
+                        <strong>VALOR: </strong>
+                        <p>R${incidents.value}</p>
+                        <button>
+                            <FiTrash2 size="20" color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
                 
             </ul>
         </div>
